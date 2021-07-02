8116642133b8152a609066f6a062af53de70a75f
Renato Oliveira Fraga <fraga@Renatos-iMac.local>
2021-07-01 20:45:34 -0500

    first commit

---
+++ Rock_Paper_Scissor WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rock_Paper_Scissor WatchKit Extension
+//
+//  Created by Renato Oliveira Fraga on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock_Paper_Scissor WatchKit Extension/Rock_Paper_ScissorApp.swift
@@ -0,0 +1,19 @@
+//
+//  Rock_Paper_ScissorApp.swift
+//  Rock_Paper_Scissor WatchKit Extension
+//
+//  Created by Renato Oliveira Fraga on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
